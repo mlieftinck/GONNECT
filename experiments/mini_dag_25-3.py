Fix for weight initialization and masking, and masked bias dimensions

experiments/mini_dag_25-3.py
@@ -6,10 +6,10 @@ import time
 from matplotlib import pyplot as plt
 from torch.utils.data import TensorDataset, DataLoader
 
-from data.dag_analysis import print_layers
+from data.DAGGenerator import DAGGenerator
+from data.dag_analysis import print_layers, create_layers
 from data.data_preprocessing import split_data
-from data.generate_masks import load_masks
-from data.go_preprocessing import construct_go_bp_layers
+from data.go_preprocessing import balance_until_convergence, pull_leaves_down
 from model.Autoencoder import Autoencoder
 from model.Decoder import SparseBIDecoder, DenseBIDecoder, Decoder
 from model.Encoder import SparseBIEncoder, Encoder, DenseBIEncoder
@@ -18,18 +18,17 @@ from train.train import train, test
 
 if __name__ == "__main__":
     # Model params
-    model_type = "dense"
-    biologically_informed = "encoder"
+    model_type = "sparse"
+    biologically_informed = "both"
     soft_links = True
     activation = torch.nn.ReLU()
-    # loss_function = mse_loss
     loss_function = mse_loss_soft_link_sum
     # GO params
     go_preprocessing = False
     merge_conditions = (1, 10)
-    n_go_layers_used = 5
+    n_go_layers_used = 500
     # Training params
-    dataset_name = "GE_top1k_bp"
+    dataset_name = "mini_dag"
     n_samples = 1000
     batch_size = 50
     n_epochs = 100
@@ -39,16 +38,15 @@ if __name__ == "__main__":
     # Model storage params
     save_weights = False
     load_weights = False
-    weights_path = "17-3"
+    weights_path = "25-3"
     # Additional params
     data_split = 0.7
     seed = 1
     dtype = torch.float64
-    n_nan_cols = 2
+    n_nan_cols = 0
 
     # Data processing
-    data = pd.read_csv(f"../../GO_TCGA/{dataset_name}.csv.gz", usecols=range(2 + min(n_samples, 11499)),
-                       compression="gzip").sort_values("gene id")
+    data = pd.DataFrame(np.random.random((3, n_samples)), columns=list("ID" + str(i) for i in range(n_samples)))
     train_set, validation_set, test_set = split_data(data, data_split, seed)
     data_np = data.iloc[:, n_nan_cols:].to_numpy()
     data_torch = TensorDataset(torch.from_numpy(np.transpose(data_np)))
@@ -61,17 +59,12 @@ if __name__ == "__main__":
     testloader = DataLoader(test_torch, batch_size=min(n_samples, batch_size), shuffle=False)
 
     # GO processing
-    if go_preprocessing:
-        print("\n----- START: GO preprocessing -----")
-        genes = list(data["gene id"])
-        go_layers = construct_go_bp_layers(genes, merge_conditions, print_go=True)
-        masks = None
-        print("----- COMPLETED: GO preprocessing -----")
-
-    else:
-        go_layers = torch.load(f"../masks/layers/{str(merge_conditions)}/{dataset_name}_layers.pt", weights_only=True)
-        masks = load_masks(biologically_informed, merge_conditions, dataset_name, model_type)
-        print("\n----- COMPLETED: Loading GO from file -----")
+    go = DAGGenerator.dag4()
+    size = len(go)
+    balance_until_convergence(go, "A")
+    pull_leaves_down(go, size)
+    go_layers = create_layers(go)
+    masks = None
 
     # Model construction
     used_go_layers = go_layers[-min(n_go_layers_used, len(go_layers)):]
@@ -105,6 +98,9 @@ if __name__ == "__main__":
         decoder = Decoder(used_go_layers, activation, dtype)
 
     model = Autoencoder(encoder, decoder)
+    # Debug
+    aa_enc_lay = model.encoder.net_layers._modules
+    aa_dec_lay = model.decoder.net_layers._modules
     if load_weights:
         model.load_state_dict(torch.load(f"../saved_weights/{dataset_name}/{weights_path}.pt", weights_only=True))
 

model/Coder.py
@@ -62,10 +62,13 @@ class DenseBICoder(DenseCoder):
                 if self.edge_masks[mask_index].device != layer.weight.device:
                     self.edge_masks[mask_index] = self.edge_masks[mask_index].to(layer.weight.device)
 
-                # Set weights towards proxies to 1, bias towards proxies to 0
+                # Set weights towards proxies to 1, bias of proxies to 0
                 layer.weight.data = torch.masked_fill(layer.weight.data, self.proxy_masks[mask_index], value=1)
-                # The [0][:] indexing acts the same as .T, but circumvents warnings about .T being deprecated
-                layer.bias.data = torch.masked_fill(layer.bias.data, self.proxy_masks[mask_index][0][:], value=0)
+                # Set bias of proxy terms to 0
+                for i in range(len(layer.bias.data)):
+                    if self.proxy_masks[mask_index][i]:
+                        layer.bias.data[i] = 0
+
                 # Set weights without edges to 0
                 layer.weight.data = torch.masked_fill(layer.weight.data, self.edge_masks[mask_index] == 0, value=0)
                 mask_index += 1
@@ -148,7 +151,9 @@ class SparseCoder(nn.Module):
                         layer.weight.data = layer.weight.data.coalesce()
                         layer.weight.data.values()[j] = 1
                 # Mask ProxyTerm bias
-                layer.bias.data = torch.masked_fill(layer.bias.data, proxy_mask.T, value=0)
+                for i in range(len(layer.bias.data)):
+                    if self.proxy_masks[mask_index][i]:
+                        layer.bias.data[i] = 0
                 mask_index += 1
 
     def _create_proxy_masks(self):

model/Decoder.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 from model.Coder import DenseCoder, DenseBICoder, SparseCoder
+from model.SparseLinear import SparseLinear
 
 
 class Decoder(DenseCoder):
@@ -17,7 +18,9 @@ class Decoder(DenseCoder):
         # (Re)initialize all weights
         for layer in self.net_layers:
             if isinstance(layer, nn.Linear):
-                nn.init.kaiming_normal_(layer.weight)
+                # Option for initialization other than standard (Kaiming uniform)
+                pass
+                # nn.init.kaiming_normal_(layer.weight)
 
 
 class DenseBIDecoder(DenseBICoder):
@@ -33,7 +36,9 @@ class DenseBIDecoder(DenseBICoder):
         # (Re)initialize all weights
         for layer in self.net_layers:
             if isinstance(layer, nn.Linear):
-                nn.init.kaiming_normal_(layer.weight)
+                # Option for initialization other than standard (Kaiming uniform)
+                pass
+                # nn.init.kaiming_normal_(layer.weight)
 
         # Mask weights using proxy and edge masks, if soft links are disabled
         if not self.soft_links:
@@ -70,8 +75,9 @@ class SparseBIDecoder(SparseCoder):
 
         # (Re)initialize all weights
         for layer in self.net_layers:
-            if isinstance(layer, nn.Linear):
-                nn.init.kaiming_normal_(layer.weight)
+            if isinstance(layer, SparseLinear):
+                # Kaiming uniform initialization for SparseTensors is implemented in the SparseLinear class, no need to re-initialize
+                pass
 
         # Mask weights using proxy masks
         self.mask_weights()

model/Encoder.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 from model.Coder import DenseCoder, DenseBICoder, SparseCoder
+from model.SparseLinear import SparseLinear
 
 
 class Encoder(DenseCoder):
@@ -19,7 +20,9 @@ class Encoder(DenseCoder):
         # (Re)initialize all weights
         for layer in self.net_layers:
             if isinstance(layer, nn.Linear):
-                nn.init.kaiming_normal_(layer.weight)
+                # Option for initialization other than standard (Kaiming uniform)
+                pass
+                # nn.init.kaiming_normal_(layer.weight)
 
 
 class DenseBIEncoder(DenseBICoder):
@@ -37,7 +40,9 @@ class DenseBIEncoder(DenseBICoder):
         # (Re)initialize all weights
         for layer in self.net_layers:
             if isinstance(layer, nn.Linear):
-                nn.init.kaiming_normal_(layer.weight)
+                # Option for initialization other than standard (Kaiming uniform)
+                pass
+                # nn.init.kaiming_normal_(layer.weight)
 
         # Mask weights using proxy and edge masks, if soft links are disabled
         if not self.soft_links:
@@ -76,8 +81,9 @@ class SparseBIEncoder(SparseCoder):
 
         # (Re)initialize all weights
         for layer in self.net_layers:
-            if isinstance(layer, nn.Linear):
-                nn.init.kaiming_normal_(layer.weight)
+            if isinstance(layer, SparseLinear):
+                # Kaiming uniform initialization for SparseTensors is implemented in the SparseLinear class, no need to re-initialize
+                pass
 
         # Mask weights using proxy masks
         self.mask_weights()