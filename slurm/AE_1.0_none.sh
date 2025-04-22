45f5eb161752f59f92fcfd2a5b1c1dc1aae5c730
mlieftinck <martijn.lieftinck@hotmail.nl>
2025-04-22 14:55:04 +0200

    New scripts for daic

--- slurm/AE_1.0_none.sh
+++ slurm/AE_1.0_none.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=ewi-insy-prb-dbl
 #SBATCH --partition=insy,general # Request partition. Default is 'general'
 #SBATCH --qos=short         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
-#SBATCH --time=2:00:00      # Request run time (wall-clock). Default is 1 minute
+#SBATCH --time=6:00:00      # Request run time (wall-clock). Default is 1 minute
 #SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
 #SBATCH --cpus-per-task=2   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
 #SBATCH --mem=10GB          # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
@@ -19,4 +19,4 @@ apptainer exec --nv --writable-tmpfs --pwd /opt/app --containall \
 	--bind src/:/opt/app/src/ \
 	--bind data/:/opt/app/data/ \
 	--bind out/:/opt/app/out/ \
-	./container_pixi_0.2.1.sif pixi run python -u src/AE_1.0_decoder.py
+	./container_pixi_0.2.1.sif pixi run python -u src/AE_1.0_none.py
