3136c80593ef98f6d4a868e5cd7aa22044cb375c
mlieftinck <martijn.lieftinck@hotmail.nl>
2025-05-28 14:19:46 +0200

    Improved soft link distribution histogram

--- src/thesis_binn/analysis/soft_links.py
+++ src/thesis_binn/analysis/soft_links.py
@@ -3,11 +3,14 @@ import torch.nn as nn
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from thesis_binn.model.Autoencoder import Autoencoder
+from thesis_binn.model.Coder import DenseCoder
 from thesis_binn.model.Encoder import DenseBICoder
 from thesis_binn.model.build_model import build_model
 
 
-def get_module_weights(module: DenseBICoder):
+def get_module_weights(module: DenseCoder):
+    """Returns a list of the weight matrices of the given module."""
     module_weights = []
     for layer in module.net_layers:
         if isinstance(layer, nn.Linear):
@@ -16,7 +19,7 @@ def get_module_weights(module: DenseBICoder):
 
 
 def get_soft_links_by_index(module: DenseBICoder):
-    """Very inefficient way to filter soft links and their indices. (Has been replaced with sparse tensors)"""
+    """DEPRECATED: Very inefficient way to filter soft links and their indices. (Has been replaced with sparse tensor implementation.)"""
     soft_link_values_per_layer = []
     soft_link_indices_per_layer = []
     layer_index = 0
@@ -49,31 +52,42 @@ def get_go_terms_by_index(module: DenseBICoder):
     return index_dicts
 
 
-def histogram_soft_links_per_layer(soft_links_per_layer, bin_width=0.01, i=None, a=1.0, model=""):
-    values = soft_links_per_layer.values()
+def histogram_weights_per_layer(weights_per_layer, bin_width=0.01, i=None, a=1.0):
+    """Assumes a sparse tensor containing the weights from a specific layer and plots the distribution of their values."""
+    values = weights_per_layer.values()
+    # Adjust number of bins so that bin width is always constant
     bin_range = max(values) - min(values)
     n_bins = int(bin_range.item() / bin_width)
-    plt.hist(soft_links_per_layer.values(), log=True, bins=n_bins, edgecolor="k", alpha=a)
+    # Plot histogram
+    _, bins, _ = plt.hist(weights_per_layer.values(), log=True, bins=n_bins, alpha=a)
     plt.xlabel("Value")
-    plt.ylabel("# Soft Links")
-    if i:
-        plt.title(f"Distribution of Soft Link values between layers {str(i)} and {str(i + 1)}\nmodel: {model}")
-    else:
-        plt.title(f"Distribution of Soft Link values between Encoder and Decoder")
-    # plt.show()
+    plt.ylabel("# Weights")
+    plt.title(f"Distribution of weight values between layers {str(i)} and {str(i + 1)}")
 
 
-def split_weights(module: DenseBICoder):
+def split_weights(module: DenseCoder):
+    """Split the weight matrices of the given module into GO and non-GO weights. If the module does not have any masks, it returns the full weight matrix as sparse tensor."""
     all_weights = get_module_weights(module)
-    masks = module.edge_masks
-    soft_links_masked = []
+    if hasattr(module, "edge_masks"):
+        masks = module.edge_masks
+    else:
+        # Model is fully connected
+        return [weight_mat.to_sparse() for weight_mat in all_weights], None
+
+    # Split weight matrices into two sparse tensors, one for soft and one for hard links
     soft_links_sparse = []
     hard_links_sparse = []
     for i in range(len(all_weights)):
-        soft_links_masked.append(torch.masked_fill(all_weights[i], masks[i], -1))
         soft_links_sparse.append(torch.masked_fill(all_weights[i], masks[i], 0).to_sparse())
         hard_links_sparse.append(torch.masked_fill(all_weights[i], ~masks[i], 0).to_sparse())
-    return soft_links_sparse, hard_links_sparse
+    return hard_links_sparse, soft_links_sparse
+
+
+def split_weights_per_module(model: Autoencoder):
+    weights = []
+    weights.append(split_weights(model.encoder))
+    weights.append(split_weights(model.decoder))
+    return weights
 
 
 if __name__ == "__main__":
@@ -81,7 +95,7 @@ if __name__ == "__main__":
     dataset_name = "TCGA_complete_bp_top1k"
     experiment_name = "AE_2.1"
     experiment_version = ".0"
-    model_name = "decoder"
+    model_name = "encoder"
     seed = 42
     n_nan_cols = 5
 
@@ -90,7 +104,7 @@ if __name__ == "__main__":
     biologically_informed = model_name  # change this for locally trained models
     soft_links = True
     random_version = None
-    go_preprocessing = True
+    go_preprocessing = False
     merge_conditions = (1, 30, 50)
     n_go_layers_used = 5
     activation_fn = torch.nn.ReLU
@@ -106,6 +120,8 @@ if __name__ == "__main__":
         genes = list(dataset.columns[n_nan_cols:])
     else:
         genes = None
+
+    # Build model
     model = build_model(model_type, biologically_informed, soft_links, dataset_name, go_preprocessing, merge_conditions,
                         n_go_layers_used, activation_fn, dtype, genes, random_version=random_version, package_call=True)
     model.load_state_dict(
@@ -113,37 +129,28 @@ if __name__ == "__main__":
             f"{project_folder}/out/trained_models/{experiment_name}/{experiment_name + experiment_version}_{model_name}_model.pt",
             weights_only=True))
 
-    module, soft_links_sparse, hard_links_sparse = None, None, None
-    soft_links_sparse_enc, soft_links_sparse_dec, hard_links_enc, hard_links_dec = None, None, None, None
-    if biologically_informed == "encoder":
-        module = model.encoder
-        soft_links_sparse, hard_links_sparse = split_weights(module)
-    elif biologically_informed == "decoder":
-        module = model.decoder
-        soft_links_sparse, hard_links_sparse = split_weights(module)
-    elif biologically_informed == "both":
-        soft_links_sparse_enc, hard_links_sparse_enc = split_weights(model.encoder)
-        soft_links_sparse_dec, hard_links_sparse_dec = split_weights(model.decoder)
+    # Histogram comparing Hard Links, Soft Links, FC
+    model_GO = build_model(model_type, biologically_informed, False, dataset_name, go_preprocessing, merge_conditions,
+                        n_go_layers_used, activation_fn, dtype, genes, random_version=random_version, package_call=True)
+    model_GO.load_state_dict(torch.load(f"{project_folder}/out/trained_models/AE_2.0/AE_2.0.0_{model_name}_model.pt", weights_only=True))
+    model_FC = build_model(model_type, "none", False, dataset_name, go_preprocessing, merge_conditions,
+                        n_go_layers_used, activation_fn, dtype, genes, random_version=random_version, package_call=True)
+    model_FC.load_state_dict(torch.load(f"{project_folder}/out/trained_models/AE_2.0/AE_2.0.0_none_model.pt", weights_only=True))
 
-    else:
-        raise ValueError(f"Unknown biologically informed module: {biologically_informed}")
+    # Get weight values for each model
+    GO_weights = split_weights_per_module(model_GO)
+    SL_weights = split_weights_per_module(model)
+    FC_weights = split_weights_per_module(model_FC)
 
-    if biologically_informed == "both":
-        edge_dicts_enc = get_go_terms_by_index(model.encoder)
-        edge_dicts_dec = get_go_terms_by_index(model.decoder)
-        for i in range(len(soft_links_sparse_enc)):
-            histogram_soft_links_per_layer(soft_links_sparse_enc[i], bin_width=0.01, a=0.5, model=model.name)
-            # Debug: overlay histogram with hard links for comparison of distributions
-            histogram_soft_links_per_layer(soft_links_sparse_dec[(n_go_layers_used-2)-i], bin_width=0.01, a=0.5, model=model.name)
-            plt.legend(["Soft links Encoder", "Soft links Decoder"])
-            plt.show()
-    else:
-        edge_dicts = get_go_terms_by_index(module)
-        for i, layer_weights in enumerate(soft_links_sparse):
-            histogram_soft_links_per_layer(layer_weights, bin_width=0.01, i=i, model=model.name)
-            # Debug: overlay histogram with hard links for comparison of distributions
-            histogram_soft_links_per_layer(hard_links_sparse[i], bin_width=0.01, i=i, a=0.5, model=model.name)
-            plt.legend(["Soft links", "Hard links"])
-            plt.show()
-
-    pass
+    module_index = 0 if model_name == "encoder" else 1
+    hard_GO = GO_weights[module_index][0]
+    soft_GO = SL_weights[module_index][0]
+    soft_FC = SL_weights[module_index][1]
+    hard_FC = FC_weights[module_index][0]
+    for i in range(n_go_layers_used - 1):
+        histogram_weights_per_layer(hard_FC[i], bin_width=0.01, i=i, a=0.2)
+        histogram_weights_per_layer(soft_FC[i], bin_width=0.01, i=i, a=0.3)
+        histogram_weights_per_layer(soft_GO[i], bin_width=0.01, i=i, a=0.3)
+        histogram_weights_per_layer(hard_GO[i], bin_width=0.01, i=i, a=0.3)
+        plt.legend(["FC Links", "Soft non-GO Links", "Soft GO Links", "Hard GO Links"])
+        plt.show()
