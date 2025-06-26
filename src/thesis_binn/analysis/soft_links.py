72f80589483534445b9af763e5da0ca383aa6adf
mlieftinck <martijn.lieftinck@hotmail.nl>
2025-06-26 15:07:09 +0200

    Soft Link histogram per module

--- src/thesis_binn/analysis/soft_links.py
+++ src/thesis_binn/analysis/soft_links.py
@@ -88,13 +88,14 @@ def split_weights(module: DenseCoder):
 
 
 def split_weights_per_module(model: Autoencoder):
+    """Split encoder and decoder weights into GO and non-GO weights based on edge masks."""
     weights = []
     weights.append(split_weights(model.encoder))
     weights.append(split_weights(model.decoder))
     return weights
 
 
-def get_top_k_soft_links(soft_links_per_layer: [torch.Tensor], k: int, layer_index: int, go_dicts: [dict]):
+def get_top_k_soft_links(soft_links_per_layer: [torch.Tensor], k: int, layer_index, go_dicts: [dict]):
     soft_links = soft_links_per_layer[layer_index]
     values = soft_links.values()
     indices = soft_links.indices()
@@ -112,25 +113,45 @@ def get_top_k_soft_links(soft_links_per_layer: [torch.Tensor], k: int, layer_ind
     return top_links
 
 
-def print_soft_links(soft_link_list: list, go: dict[GOTerm]):
+def print_soft_links(soft_link_list: list, go: dict[str, GOTerm]):
     for soft_link in soft_link_list:
         term1, term2, value = soft_link
         for term in (term1, term2):
             if isinstance(term, ProxyTerm):
                 original_term_index = term.item_id.index("_") + 1
-                term_id = term.item_id[original_term_index:]
+                term_id = term.item_id
+                name = go[term_id[original_term_index:]].name
             else:
                 term_id = term.item_id
-            name = go[term_id].name
+                name = go[term_id].name
             print(f"{term_id} -> {name}")
         print(f"Soft Link Value = {value:.3e}\n")
+
+
+def histogram_weights_per_module(module_weights, bin_width=0.01, a=1.0, color=None):
+    """Assumes a list of sparse tensors containing the weights from each layer and plots the distribution of their values."""
+    values = []
+    for i in range(len(module_weights)):
+        values += module_weights[i].values()
+
+    # Adjust number of bins so that bin width is always constant
+    bin_range = max(values) - min(values)
+    n_bins = max(1, int(bin_range.item() / bin_width))
+    # Plot histogram
+    if color:
+        _, bins, _ = plt.hist(values, log=True, bins=n_bins, alpha=a, color=color)
+    else:
+        _, bins, _ = plt.hist(values, log=True, bins=n_bins, alpha=a)
+    plt.xlabel("Value")
+    plt.ylabel("# Weights")
+    plt.title(f"Distribution of Soft Link Weights")
 
 
 if __name__ == "__main__":
     project_folder = "../../.."
     dataset_name = "TCGA_complete_bp_top1k"
-    experiment_name = "AE_3.2"
-    experiment_version = ".3"
+    experiment_name = "AE_3.1"
+    experiment_version = ".4"
     model_name = "encoder"
     n_nan_cols = 5
 
@@ -167,31 +188,48 @@ if __name__ == "__main__":
     model_GO.load_state_dict(torch.load(f"{project_folder}/out/trained_models/AE_3.-1/AE_3.-1.2_{model_name}_model.pt", weights_only=True))
 
     model_FC = build_model(model_type, "none", False, dataset_name, go_preprocessing, merge_conditions, n_go_layers_used, activation_fn, dtype, genes, random_version=random_version, package_call=True, preprocessed_go_dict=go_dict)
-    model_FC.load_state_dict(torch.load(f"{project_folder}/out/trained_models/AE_3.-1/AE_3.-1.3_none_model.pt", weights_only=True))
+    model_FC.load_state_dict(torch.load(f"{project_folder}/out/trained_models/AE_3.-1/AE_3.-1.2_none_model.pt", weights_only=True))
 
     # Get weight values for each model
     GO_weights = split_weights_per_module(model_GO)
     SL_weights = split_weights_per_module(model)
     FC_weights = split_weights_per_module(model_FC)
 
+    # Collect weights for correct module of all models to compare
     module_index = 0 if model_name == "encoder" else 1
     fixed_GO = GO_weights[module_index][0]
     soft_GO = SL_weights[module_index][0]
     soft_FC = SL_weights[module_index][1]
     fixed_FC = FC_weights[module_index][1]
-    for i in range(n_go_layers_used - 1):
-    # Debug: Plot weights of a single layer
-    # if i == 1:
-        histogram_weights_per_layer(fixed_FC[i], bin_width=0.01, i=i, a=0.2)
-        histogram_weights_per_layer(soft_FC[i], bin_width=0.01, i=i, a=0.3)
-        histogram_weights_per_layer(soft_GO[i], bin_width=0.01, i=i, a=0.3)
-        histogram_weights_per_layer(fixed_GO[i], bin_width=0.01, i=i, a=0.3)
-        plt.legend(["FC Links", "Soft non-GO Links", "Soft GO Links", "Fixed GO Links"])
-        plt.show()
 
-    # Soft Link Identification
-    index_to_go = get_go_terms_by_index(model_GO.encoder if model_name == "encoder" else model_GO.decoder)
-    layer = 0
-    soft_link_terms = get_top_k_soft_links(soft_FC, 10, 0, index_to_go)
-    print_soft_links(soft_link_terms, go_dict)
+    # # Count soft links which magnitudes exceed threshold
+    # for layer in soft_FC:
+    #     print(sum(abs(layer.values()) > 0.01).item())
+
+    # # Histogram per layer
+    # for i in range(n_go_layers_used - 1):
+    # # Debug: Plot weights of a single layer
+    # # if i == 1:
+    #     histogram_weights_per_layer(fixed_FC[i], bin_width=0.01, i=i, a=0.2)
+    #     histogram_weights_per_layer(soft_FC[i], bin_width=0.01, i=i, a=0.3)
+    #     histogram_weights_per_layer(soft_GO[i], bin_width=0.01, i=i, a=0.3)
+    #     histogram_weights_per_layer(fixed_GO[i], bin_width=0.01, i=i, a=0.3)
+    #     plt.legend(["FC Links", "Soft non-GO Links", "Soft GO Links", "Fixed GO Links"])
+    #     plt.show()
+
+    # Histogram per module
+    colors = ["#8516D1", "#1171BE", "#EDB120", "#3BAA32"]
+    histogram_weights_per_module(fixed_FC, bin_width=0.01, a=0.2, color=colors[0])
+    histogram_weights_per_module(fixed_GO, bin_width=0.01, a=0.3, color=colors[3])
+    histogram_weights_per_module(soft_GO, bin_width=0.01, a=0.4, color=colors[2])
+    histogram_weights_per_module(soft_FC, bin_width=0.01, a=0.8, color=colors[1])
+    plt.legend(["Fully Connected", "Fixed Links GO", "Soft Links (GO)", "Soft Links (non-GO)"])
+    plt.show()
+
+    # # Soft Link Identification
+    # index_to_go = get_go_terms_by_index(model_GO.encoder if model_name == "encoder" else model_GO.decoder)
+    # layer = 0 if model_name == "encoder" else 3
+    # soft_link_terms = get_top_k_soft_links(soft_FC, 10, layer, index_to_go)
+    # print_soft_links(soft_link_terms, go_dict)
+
     pass
